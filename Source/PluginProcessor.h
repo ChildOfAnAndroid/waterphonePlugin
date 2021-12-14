99e70e507d263bd44349c5b42f11ed0181f26860
Charis Cat <65819058+CharisCat@users.noreply.github.com>
2021-12-14 15:58:21 +0000

    buttonshape

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -53,7 +53,7 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
     
-    juce::midiMessageCollector& getMidiMessageCollector() noexcept (return midiMessageCollector; );
+    juce::midiMessageCollector& getMidiMessageCollector() noexcept {return midiMessageCollector; };
 
 private:
     //==============================================================================
